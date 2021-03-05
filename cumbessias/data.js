92eb3bb17c2c237de5a318ab4ba2d15f58fc2fbc
milenahj <79399802+milenahj@users.noreply.github.com>
2021-03-05 15:46:34 +0100

    ok

---
+++ cumbessias/data.js
@@ -0,0 +1,130 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010063_20210301172034",
+      "name": "R0010063_20210301172034",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.6096689944320826,
+        "pitch": 0.07134564061646742,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1875641077847554,
+          "pitch": -0.07461709651498083,
+          "rotation": 0,
+          "target": "2-r0010071_20210301174321"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.19571389174638654,
+          "pitch": -0.23207546399969026,
+          "title": "Lunetta scolpita",
+          "text": "<span style=\"color: rgb(51, 51, 51); font-family: &quot;Open Sans&quot;, Helvetica, Arial, sans-serif; font-size: 12px; background-color: rgb(255, 255, 255);\">Particolare del portale della basilica di San Gavino di Porto Torres, capolavoro dell'architettura romanico-pisana in Sardegna.&nbsp;</span><div><span style=\"color: rgb(51, 51, 51); font-family: &quot;Open Sans&quot;, Helvetica, Arial, sans-serif; font-size: 12px; background-color: rgb(255, 255, 255);\">Si tratta dell'unico portale romanico superstite, decorato con elementi provenienti da un altro portale romanico, originariamente posto sul lato meridionale, e qui spostati dai maestri catalano aragonesi nel Quattrocento.</span></div>"
+        },
+        {
+          "yaw": 2.70509853434774,
+          "pitch": -0.039279418236255026,
+          "title": "Cumbessias",
+          "text": "<span style=\"color: rgb(116, 116, 116); font-family: &quot;Open Sans&quot;, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">Le costruzioni chiamate C</span><em style=\"color: rgb(116, 116, 116); font-family: &quot;Open Sans&quot;, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">umbessias&nbsp;</em><span style=\"color: rgb(116, 116, 116); font-family: &quot;Open Sans&quot;, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">(termine che in sardo indica le case dei pellegrini) erano&nbsp; le abitazioni che ospitavano i fedeli giunti in città in occasione della festa dei Santi Martiri Turritani ed il cui impianto attuale risale ai secoli XVI-XVII.</span>"
+        },
+        {
+          "yaw": -1.9358242276275792,
+          "pitch": 0.4177705953535451,
+          "title": "Atrio Comita",
+          "text": "<span style=\"color: rgb(116, 116, 116); font-family: &quot;Open Sans&quot;, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">L' Atrio Comita è uno spazio verde all'aperto creato dal finco nord della basilica e le costruzioni chiamate&nbsp;</span><em style=\"color: rgb(116, 116, 116); font-family: &quot;Open Sans&quot;, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">cumbessias</em>"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010069_20210301174133",
+      "name": "R0010069_20210301174133",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0010071_20210301174321",
+      "name": "R0010071_20210301174321",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
