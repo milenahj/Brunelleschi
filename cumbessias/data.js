e3fea1e978c488fff17aa2fbb70542a395507393
milenahj <79399802+milenahj@users.noreply.github.com>
2021-03-10 17:03:47 +0100

    okk

---
+++ cumbessias/data.js
@@ -0,0 +1,317 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.346214622673598,
+        "pitch": -0.1425027542157551,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.02881236132474,
+          "pitch": -0.03624351468313236,
+          "rotation": 0,
+          "target": "2-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1544771974868837,
+          "pitch": -0.01422552737491145,
+          "title": "Basilica di San Gavino",
+          "text": "Unica basilica romanica con due absidi"
+        }
+      ]
+    },
+    {
+      "id": "1-1a",
+      "name": "1A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.959949344977524,
+          "pitch": -0.03477639603093152,
+          "rotation": 0,
+          "target": "2-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.2943601504872966,
+        "pitch": -0.15017308889029124,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9514289359888828,
+          "pitch": -0.02458116195441562,
+          "rotation": 0,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3679769437606524,
+          "pitch": -0.20467223742433838,
+          "title": "Interno della basilica",
+          "text": "La basilica ha uno spazio interno suddiviso in tre navate, scandite da due file di colonne romane in marmo e granito. La copertura della navata centrale è in capriate in legno."
+        }
+      ]
+    },
+    {
+      "id": "3-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.5551786236596836,
+        "pitch": -0.04459102538529436,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.718331271065633,
+          "pitch": -0.02749506502265575,
+          "rotation": 0,
+          "target": "5-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1361205433077721,
+          "pitch": -0.1324041174545414,
+          "title": "Facciata settentrionale",
+          "text": "La facciata settentrionale si dispone lungo l'atrio comita"
+        },
+        {
+          "yaw": -1.6987596326955376,
+          "pitch": 0.32105538277413714,
+          "title": "Atrio Comita",
+          "text": "è uno spazio verde all'aperto lungo il quale si affaccia la basilica e le Cumbessias"
+        },
+        {
+          "yaw": 2.7843738120009256,
+          "pitch": -0.06636240442831465,
+          "title": "Cumbessias",
+          "text": "Le Cumbessias sono delle costruzioni che ospitavano i pellegrini"
+        }
+      ]
+    },
+    {
+      "id": "4-3bportale",
+      "name": "3BPORTALE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8283943445659245,
+          "pitch": -0.08345543700824365,
+          "rotation": 0,
+          "target": "6-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.578635684545521,
+          "pitch": -0.20282284687248442,
+          "title": "Abside",
+          "text": "La basilica di San Gavino ha la particolarità di avere due absidi. Questo è quello che ospita i tre martiri all'interno."
+        }
+      ]
+    },
+    {
+      "id": "6-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.5173141823150615,
+        "pitch": -0.2983637717944774,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.135110192903287,
+          "pitch": -0.010683029594154902,
+          "rotation": 0,
+          "target": "1-1a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9625439836233856,
+          "pitch": -0.15266649345555194,
+          "title": "Abside",
+          "text": "L'abside che all'interno ospita l'altare"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
